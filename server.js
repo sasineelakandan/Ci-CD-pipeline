7f3e17e82abf92e8873d488a9b237cb9f7a1b405
sasineelakandan <devasasi887@gmail.com>
2024-10-30 14:12:35 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+
+
+const express = require('express');
+
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
